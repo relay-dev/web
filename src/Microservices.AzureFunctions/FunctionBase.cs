111d686346045af3e3ab213eb42e308222f73822
Scott Ferguson <scott.ferguson82@gmail.com>
2020-06-28 16:45:06 -0500

    Adding FunctionBase

---
+++ src/Microservices.AzureFunctions/FunctionBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Microservices.AzureFunctions
+{
+    public class FunctionBase
+    {
+        protected readonly ILogger Logger;
+
+        public FunctionBase(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        protected TResult Execute<TResult>(Func<TResult> valueFactory)
+        {
+            TResult result = default;
+
+            try
+            {
+                result = valueFactory.Invoke();
+            }
+            catch (OperationCanceledException)
+            {
+                Logger.LogInformation("Request was cancelled");
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "{0} failed with message: {1}", this.GetType(), e.Message);
+            }
+
+            return result;
+        }
+    }
+}
