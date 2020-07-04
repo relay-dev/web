f8f512c172762cb859eca61da50bee1642f37ec7
Scott Ferguson <scott.ferguson82@gmail.com>
2020-07-04 15:58:23 -0500

    Adding logging middleware for Azure Functions

---
+++ src/Microservices.AzureFunctions/Middleware/LoggerPipelineBehavior.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microservices.AzureFunctions.Middleware
+{
+    public class LoggerPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<LoggerPipelineBehavior<TRequest, TResponse>> _logger;
+
+        public LoggerPipelineBehavior(ILogger<LoggerPipelineBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (OperationCanceledException)
+            {
+                _logger.LogInformation("Request was cancelled");
+
+                throw;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "");
+
+                throw;
+            }
+        }
+    }
+}
