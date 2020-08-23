7cadb8e9a96c2a6e9a25021fd6d04913da008e6e
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-22 20:30:44 -0500

    Adding AspIntegrationTest

---
+++ src/Microservices.Testing/Integration/AspIntegrationTest.cs
@@ -0,0 +1,33 @@
+﻿using Core.Plugins.NUnit.Integration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Microservices.Testing.Integration
+{
+    public abstract class AspIntegrationTest<TToTest> : IntegrationTest<TToTest>
+    {
+        public IHostBuilder CreateTestHostBuilder<TStartup>(string basePath = "") where TStartup : class =>
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(new string[0])
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<TStartup>();
+                    webBuilder.ConfigureLogging((hostingContext, logging) =>
+                    {
+                        logging.AddConsole();
+                        logging.AddDebug();
+                    });
+                })
+                .ConfigureAppConfiguration((webBuilder, configBuilder) =>
+                {
+                    configBuilder.AddUserSecrets<TStartup>();
+
+                    configBuilder
+                        .SetBasePath(basePath)
+                        .AddJsonFile("appsettings.json", false, true)
+                        .AddJsonFile("appsettings.Development.json", false, true)
+                        .AddEnvironmentVariables();
+                });
+    }
+}
