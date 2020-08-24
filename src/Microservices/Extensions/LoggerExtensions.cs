61ce1c2a75571a7a14f7477340cd1354424ec003
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-23 21:20:35 -0500

    Adding LoggerExtensions

---
+++ src/Microservices/Extensions/LoggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Microservices.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogError(this ILogger logger, Exception e, string message, params object[] args)
+        {
+            logger.LogError(default, e, message, args);
+        }
+    }
+}
