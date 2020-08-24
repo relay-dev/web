d1227e0c93010aae2d4c77a4789f9339f7cfeee2
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-23 21:02:14 -0500

    Adding IEntityAuditor

---
+++ src/Microservices/Data/IEntityAuditor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+
+namespace Microservices.Data
+{
+    public interface IEntityAuditor
+    {
+        void Audit(IEnumerable<EntityEntry> entities);
+    }
+}
---
+++ src/Microservices/Data/Impl/EntityFrameworkEntityAuditor.cs
@@ -0,0 +1,47 @@
+﻿using Core.Framework;
+using Core.Providers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+
+namespace Microservices.Data.Impl
+{
+    public class EntityFrameworkEntityAuditor : IEntityAuditor
+    {
+        private readonly IUsernameProvider _usernameProvider;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public EntityFrameworkEntityAuditor(IUsernameProvider usernameProvider, IDateTimeProvider dateTimeProvider)
+        {
+            _usernameProvider = usernameProvider;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public void Audit(IEnumerable<EntityEntry> entities)
+        {
+            foreach (EntityEntry entry in entities)
+            {
+                string username = _usernameProvider.Get();
+                DateTime serverDateTime = _dateTimeProvider.Get();
+
+                if (entry.State == EntityState.Added)
+                {
+                    if (entry.Entity is IHaveCreatedFields entryWithCreatedFields)
+                    {
+                        entryWithCreatedFields.CreatedBy = username;
+                        entryWithCreatedFields.CreatedDate = serverDateTime;
+                    }
+                }
+                else
+                {
+                    if (entry.Entity is IHaveModifiedFields entryWithModifiedFields)
+                    {
+                        entryWithModifiedFields.ModifiedBy = username;
+                        entryWithModifiedFields.ModifiedDate = serverDateTime;
+                    }
+                }
+            }
+        }
+    }
+}
