0918db43d52d4e51048b6d6676f401215e0c658e
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 13:35:21 -0500

    Adding Diagnostics

---
+++ src/Microservices/Application/Diagnostics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Microservices.Application
+{
+    public class Diagnostics
+    {
+        /// <summary>
+        /// The unique Id of the application
+        /// </summary>
+        public long ApplicationId { get; set; }
+
+        /// <summary>
+        /// The name of the application
+        /// </summary>
+        public string ApplicationName { get; set; }
+
+        /// <summary>
+        /// The version of the application
+        /// </summary>
+        public string ApplicationVersion { get; set; }
+
+        /// <summary>
+        /// The timestamp at which this application was built
+        /// </summary>
+        public DateTime BuildTimestamp { get; set; }
+    }
+}
