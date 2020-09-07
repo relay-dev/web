51f7b57cc60ec29f4f59240beb956a979f201334
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-06 22:58:30 -0500

    Null checks for PrimitiveMappers

--- src/Microservices/Mappers/PrimitiveMappers.cs
+++ src/Microservices/Mappers/PrimitiveMappers.cs
@@ -8,8 +8,8 @@ namespace Microservices.Mappers
         public PrimitiveMappers()
         {
             // These facilitate an auto-mapper between the byte[] RowVersion on entities and the string RowVersion that is exposed on the DTO
-            CreateMap<byte[], string>().ConvertUsing(bytes => new UTF8Encoding().GetString(bytes, 0, bytes.Length));
-            CreateMap<string, byte[]>().ConvertUsing(str => new UTF8Encoding().GetBytes(str));
+            CreateMap<byte[], string>().ConvertUsing(bytes => bytes == null ? null : new UTF8Encoding().GetString(bytes, 0, bytes.Length));
+            CreateMap<string, byte[]>().ConvertUsing(str => string.IsNullOrEmpty(str) ? new byte[0] : new UTF8Encoding().GetBytes(str));
         }
     }
 }
