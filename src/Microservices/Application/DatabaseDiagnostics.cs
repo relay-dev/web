651cc527b961b3ffd30d886a62b47a4f37299680
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 14:05:24 -0500

    Adding DatabaseDiagnostics

---
+++ src/Microservices/Application/DatabaseDiagnostics.cs
@@ -0,0 +1,15 @@
+﻿namespace Microservices.Application
+{
+    public class DatabaseDiagnostics
+    {
+        /// <summary>
+        /// The name of the database server the application is connected to
+        /// </summary>
+        public string ServerName { get; set; }
+
+        /// <summary>
+        /// The name of the database the application is connected to
+        /// </summary>
+        public string DatabaseName { get; set; }
+    }
+}
