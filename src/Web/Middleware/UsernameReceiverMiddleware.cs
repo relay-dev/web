e8c5d1a35d0c279bd617a12fee11522625175154
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-26 20:22:51 -0500

    Adding UsernameReceiverMiddleware

---
+++ src/Web/Middleware/UsernameReceiverMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Core.Framework;
+using Core.Providers;
+using Extensions;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Web.Middleware
+{
+    public class UsernameReceiverMiddleware
+    {
+        private readonly ICommandContextProvider _commandContextProvider;
+        private readonly RequestDelegate _next;
+
+        public UsernameReceiverMiddleware(ICommandContextProvider commandContextProvider, RequestDelegate next)
+        {
+            _commandContextProvider = commandContextProvider;
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string username = context.Request.Headers.TryGetValueOrDefault("X-Username");
+
+            if (!string.IsNullOrEmpty(username))
+            {
+                var commandContext = new CommandContext
+                {
+                    Username = username
+                };
+
+                _commandContextProvider.Set(commandContext);
+            }
+
+            await _next(context);
+        }
+    }
+}
