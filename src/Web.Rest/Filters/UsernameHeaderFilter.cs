87034eb95d82078361bbf28c5e3880bf42c6242d
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-30 19:10:32 -0500

    Adding UsernameHeaderFilter

---
+++ src/Web.Rest/Filters/UsernameHeaderFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Net.Http;
+
+namespace Web.Rest.Filters
+{
+    public class UsernameHeaderFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (!IsWriteOperation(context))
+            {
+                return;
+            }
+
+            var openApiParameter = new OpenApiParameter
+            {
+                Name = "X-Username",
+                In = ParameterLocation.Header,
+                Required = true,
+                Example = new OpenApiString("customer@email.com")
+            };
+
+            operation.Parameters.Add(openApiParameter);
+        }
+
+        private static bool IsWriteOperation(OperationFilterContext context)
+        {
+            return string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Delete.Method, StringComparison.InvariantCultureIgnoreCase)
+                   || string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Patch.Method, StringComparison.InvariantCultureIgnoreCase)
+                   || string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Post.Method, StringComparison.InvariantCultureIgnoreCase)
+                   || string.Equals(context.ApiDescription.HttpMethod, HttpMethod.Put.Method, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
