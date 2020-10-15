5af2cf1dcad636d071c43069d9751150b3df3a49
Scott Ferguson <scott.ferguson82@gmail.com>
2020-10-15 18:38:22 -0500

    AzureFunctions wants the same

---
+++ src/Web.AzureFunctions/Configuration/AzureFunctionsConfigurationBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Web.Configuration;
+
+namespace Web.AzureFunctions.Configuration
+{
+    public class AzureFunctionsConfigurationBuilder
+    {
+        private readonly AzureFunctionsConfiguration _azureFunctionsConfiguration;
+
+        public AzureFunctionsConfigurationBuilder(WebConfigurationBuilder webConfigurationBuilder)
+        {
+            _azureFunctionsConfiguration = new AzureFunctionsConfiguration(webConfigurationBuilder.Build());
+        }
+
+        public AzureFunctionsConfiguration Build()
+        {
+            return _azureFunctionsConfiguration;
+        }
+    }
+}
---
+++ src/Web.AzureFunctions/Extensions/WebConfigurationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Web.AzureFunctions.Configuration;
+using Web.Configuration;
+
+namespace Web.AzureFunctions.Extensions
+{
+    public static class WebConfigurationBuilderExtensions
+    {
+        public static AzureFunctionsConfigurationBuilder AsAzureFunctionsConfiguration(this WebConfigurationBuilder webConfigurationBuilder)
+        {
+            return new AzureFunctionsConfigurationBuilder(webConfigurationBuilder);
+        }
+    }
+}
