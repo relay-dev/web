d2f226aff9fc66cfa12d45e27aae6dd3b8537cab
Scott Ferguson <scott.ferguson82@gmail.com>
2020-10-18 20:53:28 -0500

    Make GetPayload() an extension method

--- src/Web.AzureFunctions/Framework/EventHandlerBase.cs
+++ src/Web.AzureFunctions/Framework/EventHandlerBase.cs
@@ -6,14 +6,6 @@ namespace Web.AzureFunctions.Framework
 {
     public class EventHandlerBase : FunctionBase
     {
-        protected TPayload GetPayload<TPayload>(EventGridEvent eventGridEvent)
-        {
-            if (eventGridEvent.Data == null || string.IsNullOrEmpty(eventGridEvent.Data.ToString()))
-            {
-                throw new InvalidOperationException("EventGridEvent.Data property cannot be null or empty");
-            }
-
-            return JsonConvert.DeserializeObject<TPayload>(eventGridEvent.Data.ToString());
-        }
+        
     }
 }
