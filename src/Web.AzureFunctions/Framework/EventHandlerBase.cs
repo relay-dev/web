1a09ff2e5e3424ef19fa315c20e694254a10b3f7
Scott Ferguson <scott.ferguson82@gmail.com>
2020-10-18 20:45:46 -0500

    Adding EventHandlerBase

---
+++ src/Web.AzureFunctions/Framework/EventHandlerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json;
+using System;
+
+namespace Web.AzureFunctions.Framework
+{
+    public class EventHandlerBase : FunctionBase
+    {
+        protected TPayload GetPayload<TPayload>(EventGridEvent eventGridEvent)
+        {
+            if (eventGridEvent.Data == null || string.IsNullOrEmpty(eventGridEvent.Data.ToString()))
+            {
+                throw new InvalidOperationException("EventGridEvent.Data property cannot be null or empty");
+            }
+
+            return JsonConvert.DeserializeObject<TPayload>(eventGridEvent.Data.ToString());
+        }
+    }
+}
