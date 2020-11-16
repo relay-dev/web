18ed2ceb12a11b3187805528ffa8e59f60f459eb
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-16 15:31:00 -0600

    Adding FunctionBase.ExecuteAsync()

--- src/Web.AzureFunctions/Framework/FunctionBase.cs
+++ src/Web.AzureFunctions/Framework/FunctionBase.cs
@@ -1,10 +1,32 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Web.AzureFunctions.Framework
 {
     public class FunctionBase
     {
+        protected async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> valueFactory, ILogger logger, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await valueFactory.Invoke();
+            }
+            catch (OperationCanceledException)
+            {
+                logger.LogInformation("Request was cancelled");
+
+                throw;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "{0} failed with message: {1}", this.GetType(), e.Message);
+
+                throw;
+            }
+        }
+
         protected TResult Execute<TResult>(Func<TResult> valueFactory, ILogger logger)
         {
             try
