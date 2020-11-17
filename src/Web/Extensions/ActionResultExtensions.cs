df37df5a9afc8248900d9553b228b7396211084c
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-16 22:17:15 -0600

    Adding an overload for GetResultValue()

--- src/Web/Extensions/ActionResultExtensions.cs
+++ src/Web/Extensions/ActionResultExtensions.cs
@@ -4,6 +4,13 @@ namespace Web.Extensions
 {
     public static class ActionResultExtensions
     {
+        public static TResult GetResultValue<TResult>(this IActionResult actionResult) where TResult : class
+        {
+            var okResult = actionResult as OkObjectResult;
+
+            return okResult?.Value as TResult;
+        }
+
         public static TResult GetResultValue<TResult>(this ActionResult<TResult> actionResult) where TResult : class
         {
             var okResult = actionResult.Result as OkObjectResult;
