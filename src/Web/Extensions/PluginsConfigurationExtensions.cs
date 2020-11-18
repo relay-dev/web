b57c8255130a3ee98e03777bc976a470b5642b7f
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-18 17:43:53 -0600

    PluginsConfiguration needs to be converted to WebConfiguration

---
+++ src/Web/Extensions/PluginsConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Core.Plugins.Configuration;
+using Web.Configuration;
+
+namespace Web.Extensions
+{
+    public static class PluginsConfigurationExtensions
+    {
+        public static WebConfigurationBuilder AsWebConfiguration(this PluginConfigurationBuilder pluginConfigurationBuilder)
+        {
+            return new WebConfigurationBuilder(pluginConfigurationBuilder);
+        }
+    }
+}
