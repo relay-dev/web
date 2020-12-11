a3252f061f548c5449fcb57e601554d2e30f648c
Scott Ferguson <scott.ferguson82@gmail.com>
2020-12-11 15:42:52 -0600

    Adding ExecutionContext to AzureFunctionsIntegrationTest

--- src/Web.Testing.AzureFunctions/Integration/AzureFunctionsIntegrationTest.cs
+++ src/Web.Testing.AzureFunctions/Integration/AzureFunctionsIntegrationTest.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Web.Testing.Integration;
+using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
 namespace Web.Testing.AzureFunctions.Integration
 {
     public abstract class AzureFunctionsIntegrationTest<TToTest> : AspNetIntegrationTest<TToTest>
     {
+        protected ExecutionContext ExecutionContext => new ExecutionContext();
+
         protected IHostBuilder CreateAzureFunctionsTestHostBuilder<TStartup>() where TStartup : FunctionsStartup, new()
         {
             return new HostBuilder().ConfigureWebJobs(new TStartup().Configure);
