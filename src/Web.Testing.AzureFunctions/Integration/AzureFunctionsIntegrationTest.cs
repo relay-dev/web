0c53cfed0dd9428c42e803f6950ead675493a925
Scott Ferguson <scott.ferguson82@gmail.com>
2022-02-06 23:42:29 -0600

    Adding functionality to AzureFunctionsIntegrationTest

--- src/Web.Testing.AzureFunctions/Integration/AzureFunctionsIntegrationTest.cs
+++ src/Web.Testing.AzureFunctions/Integration/AzureFunctionsIntegrationTest.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Web.Testing.Integration;
 using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
@@ -11,7 +15,36 @@ namespace Web.Testing.AzureFunctions.Integration
 
         protected IHostBuilder CreateAzureFunctionsTestHostBuilder<TStartup>() where TStartup : FunctionsStartup, new()
         {
-            return new HostBuilder().ConfigureWebJobs(new TStartup().Configure);
+            return new HostBuilder()
+                .ConfigureWebHostDefaults(builder =>
+                {
+                    builder.ConfigureLogging((hostingContext, logging) =>
+                    {
+                        logging.AddConsole();
+                        logging.AddDebug();
+                    });
+                })
+                .ConfigureWebJobs(builder =>
+                {
+                    new TStartup().Configure(builder);
+                })
+                .ConfigureServices((ctx, services) =>
+                {
+                    ConfigureApplicationServices(services);
+                })
+                .ConfigureAppConfiguration((ctx, configBuilder) =>
+                {
+                    configBuilder
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile("appsettings.Development.json", true, true)
+                        .AddJsonFile("C:\\Azure\\appsettings.KeyVault.json", true, true)
+                        .AddJsonFile("appsettings.Local.json", true, true)
+                        .AddJsonFile("local.settings.json", true, true)
+                        .AddUserSecrets<TStartup>(true)
+                        .AddEnvironmentVariables();
+                });
         }
+
+        protected virtual void ConfigureApplicationServices(IServiceCollection services) { }
     }
 }
