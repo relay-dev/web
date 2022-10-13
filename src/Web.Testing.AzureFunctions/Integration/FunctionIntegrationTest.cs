8beb8104e8cf59c28c675561d0b83174a931975f
Scott Ferguson <scott.ferguson82@gmail.com>
2022-10-13 08:24:43 -0500

    Cleanup

--- src/Web.Testing.AzureFunctions/Integration/FunctionIntegrationTest.cs
+++ src/Web.Testing.AzureFunctions/Integration/FunctionIntegrationTest.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Logging;
 using System;
 using Web.Testing.Integration;
-using ExecutionContext = Microsoft.Azure.WebJobs.ExecutionContext;
 
 namespace Web.Testing.AzureFunctions.Integration
 {
